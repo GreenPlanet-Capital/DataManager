91d012aa379aac4f390f882a3608669ab0f3c2ea
Atharva Kale <atharvakale343@gmail.com>
2022-02-21 22:22:13 -0500

    Installable changes

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='DataManager',
+    version='0.0.0',
+    description='A library to manage and return financial asset data using Alpaca',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n',
+    license='Proprietary',
+    packages=find_packages(),
+    author='Green Planet Capital',
+    author_email='greenplanetcap.unofficial@gmail.com',
+    keywords=['GPC', 'GreenPlanetCapital'],
+    url='https://github.com/GreenPlanet-Capital/DataManager',
+    download_url='https://github.com/GreenPlanet-Capital/DataManager',
+    include_package_data=True,
+    
+)
+
+install_requires = [
+    'wheel',
+    'dataset',
+    'pytest',
+    'iexfinance',
+    'alpaca-trade-api',
+    'pandas_market_calendars',
+    'lxml',
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
